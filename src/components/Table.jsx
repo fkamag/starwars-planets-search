827eec6b0e0c86c4465e9793457e3f27554961d7
fkamag <fabiokamagian@gmail.com>
2022-07-16 00:20:48 -0300

    requisito 1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import MyContext from '../context/MyContext';
 
 function Table() {
+  const { listPlanets } = useContext(MyContext);
+  console.log(listPlanets);
   return (
     <table className="table">
       <thead>
-        <th>Name</th>
-        <th>Rotation Period</th>
-        <th>Orbital Period</th>
-        <th>Diameter</th>
-        <th>Climate</th>
-        <th>Gravity</th>
-        <th>Terrain</th>
-        <th>Surface Water</th>
-        <th>Population</th>
-        <th>Films</th>
-        <th>Created</th>
-        <th>Edited</th>
-        <th>URL</th>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
       </thead>
+      <tbody>
+        {
+          listPlanets.map((planet) => (
+            <tr key={ planet.name }>
+              <td>{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.films }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.url }</td>
+            </tr>
+          ))
+        }
+      </tbody>
     </table>
   );
 }
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './MyContext';
 
 function Provider({ children }) {
-  const [state] = useState([]);
+  const [listPlanets, setListPlanets] = useState([]);
+
+  useEffect(() => {
+    const getListPlanets = async () => {
+      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const result = await response.json();
+      const data = result.results;
+      data.forEach((planet) => {
+        delete planet.residents;
+      });
+      console.log(data);
+      setListPlanets(data);
+    };
+    getListPlanets();
+  }, []);
+
+  const context = {
+    listPlanets,
+    setListPlanets,
+  };
 
   return (
-    <MyContext.Provider value={ state }>
+    <MyContext.Provider value={ context }>
       { children }
     </MyContext.Provider>
   );
