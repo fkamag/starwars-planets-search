da470da49cbb84f3e20026cbf410d390605d42f0
fkamag <fabiokamagian@gmail.com>
2022-07-30 20:16:27 -0300

    incluindo testes no FilterByNumber

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 test('Page.js', () => {
@@ -12,4 +13,28 @@ test('FilterByName', () => {
   render(<App />);
   const searchElement = screen.getByPlaceholderText('Search here')
   expect(searchElement).toBeInTheDocument();
+  expect(searchElement).toHaveValue('');
+  userEvent.type(searchElement, 'Tatooine');
+  expect(searchElement).toHaveValue('Tatooine');
+});
+
+test('FilterByNumber', () => {
+  render(<App />);
+  const columnSearch = screen.getByTestId('column-filter');
+  expect(columnSearch).toBeInTheDocument();
+  expect(columnSearch).toHaveValue('population');
+  userEvent.selectOptions(columnSearch, 'diameter');
+  expect(columnSearch).toHaveValue('diameter');
+
+  const comparisonSearch = screen.getByTestId('comparison-filter');
+  expect(comparisonSearch).toBeInTheDocument();
+  expect(comparisonSearch).toHaveValue('maior que');
+  userEvent.selectOptions(comparisonSearch, 'igual a');
+  expect(comparisonSearch).toHaveValue('igual a');
+
+  const valueSearch = screen.getByTestId('value-filter');
+  expect(valueSearch).toBeInTheDocument();
+  expect(valueSearch).toHaveValue(0);
+  userEvent.type(valueSearch, '10');
+  expect(valueSearch).toHaveValue(10);
 });
