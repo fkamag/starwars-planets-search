ffd5f62637374d4a315bf1a1ebbb187879853af1
fkamag <fabiokamagian@gmail.com>
2022-07-31 23:32:24 -0300

    incluindo mock e mais alguns testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,72 +2,88 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
+import MockAPI from './MockAPI';
 
-test('Page.js', () => {
-  render(<App />);
-  const imgElement = screen.getByRole('img', {name: 'logo'});
-  expect(imgElement).toBeInTheDocument();
-});
+describe('Testando projeto Starwars', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch')
+      .mockResolvedValue({ json: jest.fn().mockResolvedValue(MockAPI) });
+  });
 
-test('FilterByName', () => {
-  render(<App />);
-  const searchElement = screen.getByPlaceholderText('Search here')
-  expect(searchElement).toBeInTheDocument();
-  expect(searchElement).toHaveValue('');
-  userEvent.type(searchElement, 'Tatooine');
-  expect(searchElement).toHaveValue('Tatooine');
-});
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-test('FilterByNumber', () => {
-  render(<App />);
-  const columnSearch = screen.getByTestId('column-filter');
-  expect(columnSearch).toBeInTheDocument();
-  expect(columnSearch).toHaveValue('population');
-  userEvent.selectOptions(columnSearch, 'diameter');
-  expect(columnSearch).toHaveValue('diameter');
+  test('Page.js', () => {
+    render(<App />);
+    const imgElement = screen.getByRole('img', {name: 'logo'});
+    expect(imgElement).toBeInTheDocument();
+  });
 
-  const comparisonSearch = screen.getByTestId('comparison-filter');
-  expect(comparisonSearch).toBeInTheDocument();
-  expect(comparisonSearch).toHaveValue('maior que');
-  userEvent.selectOptions(comparisonSearch, 'menor que');
-  expect(comparisonSearch).toHaveValue('menor que');
+  test('FilterByName', () => {
+    render(<App />);
+    const searchElement = screen.getByPlaceholderText('Search here')
+    expect(searchElement).toBeInTheDocument();
+    expect(searchElement).toHaveValue('');
+    userEvent.type(searchElement, 'Tatooine');
+    expect(searchElement).toHaveValue('Tatooine');
+  });
 
-  const valueSearch = screen.getByTestId('value-filter');
-  expect(valueSearch).toBeInTheDocument();
-  expect(valueSearch).toHaveValue(0);
-  userEvent.type(valueSearch, '10000');
-  expect(valueSearch).toHaveValue(10000);
+  test('FilterByNumber', () => {
+    render(<App />);
+    const columnSearch = screen.getByTestId('column-filter');
+    expect(columnSearch).toBeInTheDocument();
+    expect(columnSearch).toHaveValue('population');
+    userEvent.selectOptions(columnSearch, 'diameter');
+    expect(columnSearch).toHaveValue('diameter');
 
-  const buttonFilter = screen.getByTestId('button-filter');
-  expect(buttonFilter).toBeInTheDocument();
-  userEvent.click(buttonFilter);
+    const comparisonSearch = screen.getByTestId('comparison-filter');
+    expect(comparisonSearch).toBeInTheDocument();
+    expect(comparisonSearch).toHaveValue('maior que');
+    userEvent.selectOptions(comparisonSearch, 'menor que');
+    expect(comparisonSearch).toHaveValue('menor que');
 
-  const listFilters = screen.getByTestId('filter');
-  expect(listFilters).toBeInTheDocument();
-  
-  const textListFilters = screen.getAllByText('diameter');
-  expect(textListFilters).toHaveLength(2);
-});
+    const valueSearch = screen.getByTestId('value-filter');
+    expect(valueSearch).toBeInTheDocument();
+    expect(valueSearch).toHaveValue(0);
+    userEvent.type(valueSearch, '10000');
+    expect(valueSearch).toHaveValue(10000);
 
-test('Sort', () => {
-  render(<App />);
-  const columnSort = screen.getByTestId('column-sort');
-  expect(columnSort).toBeInTheDocument();
-  expect(columnSort).toHaveValue('population');
-  userEvent.selectOptions(columnSort, 'diameter');
-  expect(columnSort).toHaveValue('diameter');
+    const buttonFilter = screen.getByTestId('button-filter');
+    expect(buttonFilter).toBeInTheDocument();
+    userEvent.click(buttonFilter);
 
-  const ascOption = screen.getByTestId('column-sort-input-asc');
-  const descOption = screen.getByTestId('column-sort-input-desc');
-  expect(ascOption).toBeInTheDocument();
-  expect(descOption).toBeInTheDocument();
+    const listFilters = screen.getByTestId('filter');
+    expect(listFilters).toBeInTheDocument();
+    
+    const textListFilters = screen.getAllByText('diameter');
+    expect(textListFilters).toHaveLength(2);
+  });
 
-  const buttonSort = screen.getByTestId('column-sort-button');
-  expect(buttonSort).toBeInTheDocument();
-});
+  test('Sort', () => {
+    render(<App />);
+    const columnSort = screen.getByTestId('column-sort');
+    expect(columnSort).toBeInTheDocument();
+    expect(columnSort).toHaveValue('population');
+    userEvent.selectOptions(columnSort, 'diameter');
+    expect(columnSort).toHaveValue('diameter');
 
-test('Table', () => {
-  render(<App />);
-  const theadTable = screen.getByText('Name');
-  expect(theadTable).toBeInTheDocument();
-});
+    const ascOption = screen.getByTestId('column-sort-input-asc');
+    const descOption = screen.getByTestId('column-sort-input-desc');
+    expect(ascOption).toBeInTheDocument();
+    expect(descOption).toBeInTheDocument();
+
+    const buttonSort = screen.getByTestId('column-sort-button');
+    expect(buttonSort).toBeInTheDocument();
+  });
+
+  test('Table and API call', async () => {
+    render(<App />);
+    const theadTable = screen.getByText('Name');
+    expect(theadTable).toBeInTheDocument();
+
+    const itemTable = await screen.findByText('Tatooine');
+    expect(itemTable).toBeInTheDocument();
+    expect(global.fetch).toBeCalledTimes(1);
+  });
+})
